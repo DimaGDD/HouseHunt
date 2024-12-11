06e99af5ce289f51ca20e20148fa972b618d2396
DimaGDD <dimagelenidze21@gmail.com>
2024-12-12 01:22:31 +0300

    Clear

--- tg_bot/app/keaboards.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
-
-
-start_keaboard = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [InlineKeyboardButton(text='▶️ Open WebApp', web_app=WebAppInfo(url='https://dimagdd.github.io/HouseHunt/'))]
-    ]
-)
