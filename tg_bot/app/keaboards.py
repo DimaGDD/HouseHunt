aa18f5d89432256cb7ca200b409031b5414c5af7
DimaGDD <dimagelenidze21@gmail.com>
2024-12-11 22:03:33 +0300

    Hosting

--- tg_bot/app/keaboards.py
+++
@@ -1,5 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
-
-start_keaboard = InlineKeyboardMarkup(
-	inline_keyboard=[InlineKeyboardButton(text='Узнать цену', callback_data='get_price')]
-)
