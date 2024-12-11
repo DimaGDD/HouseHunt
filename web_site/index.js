5593f6edb1f26e43e4633da3926d78da952e8785
ThomasRayDev <ilchenko290@gmail.com>
2024-12-11 20:33:50 +0300

    backend for web site

---
+++ web_site/index.js
@@ -0,0 +1,43 @@
+const form = document.querySelector('.model-form');
+const predictButton = document.querySelector('.model-btn');
+
+const API_URL = 'http://127.0.0.1:5000/api/data';
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const district = document.querySelector('#district').value;
+  const rooms = document.querySelector('#rooms').value;
+  const area = document.querySelector('#area').value;
+  const floor = document.querySelector('#floor').value;
+
+  const requestData = {
+    place: district,
+    room_number: rooms,
+    squares: area,
+    floor: floor,
+  };
+
+  console.log('Отправка данных: ', requestData);
+
+  try {
+    const response = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestData),
+    });
+
+    if (response.ok) {
+      const result = await response.json();
+
+      alert(`Предсказанная стоимость: ${result.cost} руб.`);
+    } else {
+      alert('Ошибка при предсказании. Попробуйте позже.');
+    }
+  } catch (error) {
+    console.log(error);
+    alert('Не удалось связаться с сервером. Ошибку см. в консоли.');
+  }
+});
---
+++ web_site/main.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import joblib
+
+app = Flask(__name__)
+CORS(app)
+model = joblib.load('apartment_cost.pkl')
+
+@app.route('/api/data', methods=['POST'])
+def handle_post():
+  try:
+    data = request.get_json()
+    if not data:
+      return jsonify({'error': 'No data provided'}), 400
+    
+    place = [0, 0, 0, 0, 0, 0, 0]
+    place[int(data['place'])] = 1
+
+    model_data = [
+      [int(data['room_number']), int(data['squares']), int(data['floor'])] + place
+    ]
+
+    print(model_data)
+
+    predicted_cost = model.predict(model_data)
+
+    return jsonify({ 'cost': str(predicted_cost[0]) }), 200
+  except Exception as e:
+    return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+  app.run(debug=True)
